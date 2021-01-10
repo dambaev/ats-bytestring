010ef65e9b9f49d9da513c0dcf58f261767cac22
Dambaev Alexander <ice.redmine@gmail.com>
2021-01-11 06:01:38 +0800

    fix shell.nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,13 @@
-{ nixpkgs ? import <nixpkgs> { }}:
 let
-  inherit (nixpkgs) pkgs;
-  project = import ./release.nix {};
-in
-project.env
+#  nixpkgs = import <nixpkgs>;
+  pkgs = import <nixpkgs> {
+    config = {};
+    overlays = [
+      (import ./overlay.nix)
+    ];
+  };
+  shell = pkgs.mkShell {
+    buildInputs = pkgs.ats-bytestring.buildInputs ++ [ pkgs.valgrind pkgs.gdb ];
+  };
+
+in shell
